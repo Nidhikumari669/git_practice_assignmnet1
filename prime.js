787f5e4bc11d53a0d314bb108efc9f98d5624ece
Nidhikumari669 <nv256947@gmail.com>
2023-02-11 22:38:50 +0530

    I removed the logic inside

--- prime.js
+++ prime.js
@@ -1,8 +1,7 @@
 let number=15;
 let factor=0;
 for(let i=0;i<=number;i++){
-if(number%i==0){
-factor++;
+git_practice_assignmnet1
 }
 }
 (factor==2) ? console.log("it's a prime number") : console.log("it is not a prime number");
